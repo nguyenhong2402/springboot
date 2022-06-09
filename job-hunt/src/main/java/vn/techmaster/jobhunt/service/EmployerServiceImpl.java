1b96fe1ef63c6d662454d1a532f37257894ff793
nguyenhong2402 <nguyenxuanhong2402@gmail.com>
2022-06-09 20:46:17 +0700

    edit

--- job-hunt/src/main/java/vn/techmaster/jobhunt/service/EmployerServiceImpl.java
+++ job-hunt/src/main/java/vn/techmaster/jobhunt/service/EmployerServiceImpl.java
@@ -3,7 +3,7 @@ package vn.techmaster.jobhunt.service;
 import java.util.List;
 import java.util.UUID;
 
-import javax.validation.Valid;
+
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
